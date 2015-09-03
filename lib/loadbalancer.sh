e52bb3de5b69cf317a8e393efc7194a69b04256b
Glenn Morton <glenn@sandcastle.io>
2015-09-03 09:07:56 +0800

    updated elb config

--- lib/loadbalancer.sh
+++ lib/loadbalancer.sh
@@ -5,41 +5,49 @@
 # --------------------------------------
 # ELB
 
-# create ELB
-ELB="elb-${APP_NAME}"
-ELB_ID=$(aws elb create-load-balancer \
+ELB_ID=
+
+# Creates a load balancer for the cluster
+# usage: createLoadBalancer
+createLoadBalancer() {
+
+  # create ELB
+  local elb="elb-${APP_NAME}"
+  ELB_ID=$(aws elb create-load-balancer \
+        --load-balancer-name "${ELB}" \
+        --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" \
+        --subnets "${SUBNET_ELB_A_ID}" "${SUBNET_ELB_B_ID}" \
+        --security-groups "${GRP_ELB_ID}" \
+        --output text)
+
+  echo ""
+  echo "[Load Balancer]"
+  echo " - ${ELB_ID} / ${ELB} (80->80)"
+
+  # -------
+
+  # enable cross zone load balancing
+  $(aws elb modify-load-balancer-attributes \
       --load-balancer-name "${ELB}" \
-      --listeners Protocol=HTTP,LoadBalancerPort=80,InstancePort=80 \
-      --availability-zones= "${AWS_REGION}${AWS_ZONE_1}" "${AWS_REGION}${AWS_ZONE_1}" \ 
-      --subnets "${SUBNET_ELB_A_ID}" "${SUBNET_ELB_B_ID}" \
-      --security-groups "${GRP_ELB_ID}" \
-      --output text)
+      --load-balancer-attributes "{\"CrossZoneLoadBalancing\":{\"Enabled\":true}}")
 
-# --------------------------------------
-# ELB Config
+  # -------
 
-# enable cross zone load balancing
-aws elb modify-load-balancer-attributes \
-    --load-balancer-name ${ELB} \
-    --load-balancer-attributes "{\"CrossZoneLoadBalancing\":{\"Enabled\":true}}"
+  # ELB Proxy Protocol (Websocket Support)
+  local proxy_policy="${ELB}-proxy-protocol"
 
-# define proxy protocol policy
-aws elb create-load-balancer-policy \
-    --load-balancer-name $ELB \
-    --policy-name $ELB-proxy-protocol \
-    --policy-type-name ProxyProtocolPolicyType \
-    --policy-attributes AttributeName=ProxyProtocol,AttributeValue=True
+  # define policy
+  aws elb create-load-balancer-policy \
+      --load-balancer-name "${ELB}" \
+      --policy-name "${proxy_policy}" \
+      --policy-type-name "ProxyProtocolPolicyType" \
+      --policy-attributes "AttributeName=ProxyProtocol,AttributeValue=true"
 
-# attach policy to elb
-aws elb set-load-balancer-policies-for-backend-server \
-    --load-balancer-name $ELB_NAME \
-    --instance-port 80 \
-    --policy-names $ELB_NAME-proxy-protocol
+  # apply policy
+  aws elb set-load-balancer-policies-for-backend-server \
+      --load-balancer-name "${ELB}" \
+      --instance-port 80 \
+      --policy-names "${proxy_policy}"
+}
 
-# --------------------------------------
-# ELB Health Checks
-
-# enable health checks
-# aws elb configure-health-check \
-#     --load-balancer-name mp1jrh3 \
-#     --health-check Target=HTTP:80/index.php,Interval=30,Timeout=5,UnhealthyThreshold=2,HealthyThreshold=10
+createLoadBalancer
